7e2ba1bdcaeeefe33b5cad8281fc183e1a4cad6f
Jacob Fairhead <57509628+JacobFairhead@users.noreply.github.com>
2023-05-20 15:18:38 +0100

    /way working

--- Waypoint.lua
+++ Waypoint.lua
@@ -1,34 +1,34 @@
-local _ = ...;
-
-SLASH_UtilitiesPlus1 = '/way';
+SLASH_Waypoint1 = '/way'
 
 function Waypoint_HelpText()
+  print('------ UtilitiesPlus - Waypoint Help ------')
   print('Command requires 2 numbers between 0 - 100')
   print('For example: /way 25 30')
   print('To remove existing waypoint use: /way clear')
+  print('-------------------------------------------')
 end
 
 function SplitStr(str)
-  local t = {}
-  for s in str.gmatch(str, '([^%s]+)') do
-    table.insert(t, s)
+  local tbl = {}
+  for x in str:gmatch('%d+') do
+    table.insert(tbl, x)
   end
-  return t
+  return tbl
 end
 
 function Waypoint_Set(cords)
   if (#cords == 2 and cords[1] <= 1 and cords[2] <= 1) then
     local location = C_Map.GetBestMapForUnit('player')
     C_Map.SetUserWaypoint(UiMapPoint.CreateFromCoordinates(location, cords[1], cords[2]))
-    local link = C_Map.GetUserWaypointHyperLink()
-    print(string.format('Waypoint created at (x: %i, y: %i) : %s', (cords[1] * 100), (cords[2] * 100), link))
+    local link = C_Map.GetUserWaypointHyperlink()
+    print(string.format('Waypoint created at (x: %d, y: %d) : ', (cords[1] * 100), (cords[2] * 100)) .. link)
     C_SuperTrack.SetSuperTrackedUserWaypoint(true)
   else
     Waypoint_HelpText()
   end
 end
 
-function SlashCmdList.Waypoint(msg, _)
+function SlashCmdList.Waypoint(msg)
   if msg == 'clear' then
     C_Map.ClearUserWaypoint()
     C_SuperTrack.SetSuperTrackedUserWaypoint(false)
@@ -37,14 +37,12 @@ function SlashCmdList.Waypoint(msg, _)
     local options = {}
     local terms = SplitStr(msg)
     for _, value in pairs(terms) do
-      value = value:gsub('%.', '')
-      value = value:gsub('%,', '')
-      value = string.match(value, '^(%d+)$')
+      value = value:gsub('%.', ''):gsub('%,', ''):match('^(%d+)$')
       if value then
-        if string.find(value, '%.') or string.find(value, '%.') then
-          value = (value / (10 ^ string.len(value)))
+        if value:find('%.') or value:find('%,') then
+          value = (value / (10 ^ value:len()))
         else
-          value(value / (10 ^ 2))
+          value = (value / (10 ^ 2))
         end
         table.insert(options, value)
       end
